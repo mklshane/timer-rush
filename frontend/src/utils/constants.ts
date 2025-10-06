78e7a7fb571f33134e2caa2d8bc80538a51cf019
Mikaela Shane Estrellanes <mikaela_shane_estrellanes@dlsl.edu.ph>
2025-10-06 21:39:04 +0800

    slsl

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -3,4 +3,4 @@ export const BASE_URL = "https://timerrush-server.onrender.com/api";
 
 /* export const BASE_URL = 'http://localhost:3000/api' */
 
-//  "https://timerrush-server.onrender.com/api"
+//  
