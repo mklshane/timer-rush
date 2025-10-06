3a111413aee28b381573b3418f7d0270eff56af6
Mikaela Shane Estrellanes <mikaela_shane_estrellanes@dlsl.edu.ph>
2025-10-06 21:52:49 +0800

    dksk

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()],
-})
+  plugins: [react(),
+    tailwindcss()
+  ],
+  build: {
+    outDir: "dist",
+    sourcemap: false, 
+  },
+  server: {
+    port: 5173,
+    host: true,
+  },
+  base: "./",
+});
